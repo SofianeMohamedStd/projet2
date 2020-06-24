736f7d9da8274c2e9ea789c7019210fe6e80e1aa
SofinaeMohamedStd <namoune.sofianemohamed@gmail.com>
2020-06-24 21:34:36 +0200

    test leaflet

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+function init(){
+    
+    const o = {lat: 47.752045, lng: 7.316796};
+    const zoomLevel = 20;
+
+    const map = L.map('maptest').setView([o.lat, o.lng], zoomLevel);
+
+    const mainLayer= L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1Ijoic29maWFuZW4iLCJhIjoiY2tidHF2MGVuMGNqMjJ6bGNtbjJydzc3YSJ9._vwvrngqV0ioWkXni3XQIw'
+    });
+    mainLayer.addTo(map);
+}
+
