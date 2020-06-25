f0acedd27ce33f3b66ce81895e527e5312f3cc08
SofinaeMohamedStd <namoune.sofianemohamed@gmail.com>
2020-06-25 12:00:43 +0200

    section 7 8

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ function init(){
     const o = {lat: 47.752045, lng: 7.316796};
     const zoomLevel = 20;
 
-    const map = L.map('maptest').setView([o.lat, o.lng], zoomLevel);
+    const map = L.map('section7').setView([o.lat, o.lng], zoomLevel);
 
     const mainLayer= L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
