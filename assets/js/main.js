46c2ae41dd6aa8c71c4d76891753d02a3c9d7ca8
SofinaeMohamedStd <namoune.sofianemohamed@gmail.com>
2020-06-26 08:30:23 +0200

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ function init(){
     const o = {lat: 47.752045, lng: 7.316796};
     const zoomLevel = 20;
 
-    const map = L.map('section7').setView([o.lat, o.lng], zoomLevel);
+    const map = L.map('mapp').setView([o.lat, o.lng], zoomLevel);
 
     const mainLayer= L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -12,7 +12,11 @@ function init(){
         tileSize: 512,
         zoomOffset: -1,
         accessToken: 'pk.eyJ1Ijoic29maWFuZW4iLCJhIjoiY2tidHF2MGVuMGNqMjJ6bGNtbjJydzc3YSJ9._vwvrngqV0ioWkXni3XQIw'
+    
     });
-    mainLayer.addTo(map);
+    const a=document.getElementById("overly").style.display = "block";
+
+    mainLayer.addTo(map,overly);
 }
+
 
